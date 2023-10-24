70770170d8b07503f38caa65dab7124dd69242be
RobertWright0830 <dnafrankenstein@gmail.com>
2023-10-24 08:21:20 -0500

    reformatted indentation

--- index.js
+++ index.js
@@ -1,79 +1,93 @@
 // Packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // Array of questions for user input
 const questions = [
-    {
-        type: 'input',
-        message: 'What is the project title?\n',
-        name: 'title',
-    },
-    {
-        type: 'input',
-        message: 'DESCRIPTION with motivation, why you built it, and what it solves:\n ',
-        name: 'description',
-    },
-    {
-        type: 'input', 
-        message: 'Provide INSTALLATION steps:\n ',
-        name: 'installation',
-    },
-    {
-        type: 'input',
-        message: 'Provide USAGE instructions:\n ',
-        name: 'usage',
-    },
-    {
-        type: 'input',
-        message: 'Provide CREDITS, including GitHub profiles of collaborators, third-party assets or tutorial links: \n',
-        name: 'credits',
-    },
-    {
-        type: 'input', 
-        message: 'Include recommended TESTS and examples on how to run them: \n',
-        name: 'tests',
-    },
-    {
-        type: 'list',
-        message: 'Select a license for your application: \n',
-        choices: ['Apache-2.0', 'GPL-3.0', 'MIT', 'BSD-2-Clause', 'BSD-3-Clause', 'BSL-1.0', 'EPL-2.0', 'AGPL-v3', 'GPL-2.0', 'LGPL-2.1', 'MPL-2.0', 'Unlicense', 'None'],
-        name: 'license',
-    },
-    {
-        type: 'input',
-        message: 'Enter your GitHub username: \n',
-        name: 'githubUsername',
-    },
-    {
-        type: 'input',
-        message: 'Enter your email address: \n',
-        name: 'email',
-    },
+  {
+    type: "input",
+    message: "What is the project title?\n",
+    name: "title",
+  },
+  {
+    type: "input",
+    message:
+      "DESCRIPTION with motivation, why you built it, and what it solves:\n ",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "Provide INSTALLATION steps:\n ",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "Provide USAGE instructions:\n ",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message:
+      "Provide CREDITS, including GitHub profiles of collaborators, third-party assets or tutorial links: \n",
+    name: "credits",
+  },
+  {
+    type: "input",
+    message: "Include recommended TESTS and examples on how to run them: \n",
+    name: "tests",
+  },
+  {
+    type: "list",
+    message: "Select a license for your application: \n",
+    choices: [
+      "Apache-2.0",
+      "GPL-3.0",
+      "MIT",
+      "BSD-2-Clause",
+      "BSD-3-Clause",
+      "BSL-1.0",
+      "EPL-2.0",
+      "AGPL-v3",
+      "GPL-2.0",
+      "LGPL-2.1",
+      "MPL-2.0",
+      "Unlicense",
+      "None",
+    ],
+    name: "license",
+  },
+  {
+    type: "input",
+    message: "Enter your GitHub username: \n",
+    name: "githubUsername",
+  },
+  {
+    type: "input",
+    message: "Enter your email address: \n",
+    name: "email",
+  },
 ];
 
 //Function to write README file
 function writeToFile(title, data) {
-    const randomNumber = Math.floor(Math.random() * 10000);
-    const folderName = `${title}_${randomNumber}`
-    const fileName = `${folderName}/README.md`;
-    
-    fs.mkdirSync(folderName);
+  const randomNumber = Math.floor(Math.random() * 10000);
+  const folderName = `${title}_${randomNumber}`;
+  const fileName = `${folderName}/README.md`;
 
-    fs.writeFile(fileName, data, (err) =>
-    err ? console.error(err) : console.log('Success!')
-    );
+  fs.mkdirSync(folderName);
+
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log("Success!")
+  );
 }
 
 // Function to initialize app
 function init() {
-    inquirer
-        .prompt(questions)
-        .then((responses) => {
-            const readmeInformation = generateMarkdown(responses);
-            writeToFile(responses.title, readmeInformation);
-        });
+  inquirer.prompt(questions).then((responses) => {
+    const readmeInformation = generateMarkdown(responses);
+    writeToFile(responses.title, readmeInformation);
+  });
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,8 @@
 //Function that returns a license badge based on which license is passed in
 // If there is no license, returns an empty string
 function renderLicenseBadge(license) {
-  if (license === 'None') {
-    return '';
+  if (license === "None") {
+    return "";
   }
   return `![License](https://img.shields.io/badge/License-${license}-blue.svg)`;
 }
@@ -10,8 +10,8 @@ function renderLicenseBadge(license) {
 // Function that returns the license link
 // If there is no license, returns an empty string
 function renderLicenseLink(license) {
-  if (license === 'None') {
-    return '';
+  if (license === "None") {
+    return "";
   }
   return `(https://opensource.org/license/${license})`;
 }
@@ -19,8 +19,8 @@ function renderLicenseLink(license) {
 // Function that returns the license section of README
 // If there is no license, returns an empty string
 function renderLicenseSection(license) {
-  if (license === 'None') {
-    return 'This project is not covered by any license.';
+  if (license === "None") {
+    return "This project is not covered by any license.";
   }
   return `This project is licensed under [${license} license](https://opensource.org/license/${license}).`;
 }
@@ -29,35 +29,35 @@ function renderLicenseSection(license) {
 function generateMarkdown(data) {
   let licenseBadge;
   switch (data.license) {
-    case 'Apache-2.0':
-      licenseBadge = renderLicenseBadge('Apache_2.0');
+    case "Apache-2.0":
+      licenseBadge = renderLicenseBadge("Apache_2.0");
       break;
-    case 'GPL-3.0':
-      licenseBadge = renderLicenseBadge('GPL_3.0');
+    case "GPL-3.0":
+      licenseBadge = renderLicenseBadge("GPL_3.0");
       break;
-    case 'BSD-2-Clause':
-      licenseBadge = renderLicenseBadge('BSD_2_Clause');
+    case "BSD-2-Clause":
+      licenseBadge = renderLicenseBadge("BSD_2_Clause");
       break;
-    case 'BSD-3-Clause':
-      licenseBadge = renderLicenseBadge('BSD_3_Clause');
+    case "BSD-3-Clause":
+      licenseBadge = renderLicenseBadge("BSD_3_Clause");
       break;
-    case 'BSL-1.0':
-      licenseBadge = renderLicenseBadge('BSL_1.0');
+    case "BSL-1.0":
+      licenseBadge = renderLicenseBadge("BSL_1.0");
       break;
-    case 'EPL-2.0':
-      licenseBadge = renderLicenseBadge('EPL_2.0');
+    case "EPL-2.0":
+      licenseBadge = renderLicenseBadge("EPL_2.0");
       break;
-    case 'AGPL-v3':
-      licenseBadge = renderLicenseBadge('AGPL_v3');
+    case "AGPL-v3":
+      licenseBadge = renderLicenseBadge("AGPL_v3");
       break;
-    case 'GPL-2.0':
-      licenseBadge = renderLicenseBadge('GPL_2.0');
+    case "GPL-2.0":
+      licenseBadge = renderLicenseBadge("GPL_2.0");
       break;
-    case 'LGPL-2.1':
-      licenseBadge = renderLicenseBadge('LGPL_2.1');
+    case "LGPL-2.1":
+      licenseBadge = renderLicenseBadge("LGPL_2.1");
       break;
-    case 'MPL-2.0':
-      licenseBadge = renderLicenseBadge('MPL_2.0');
+    case "MPL-2.0":
+      licenseBadge = renderLicenseBadge("MPL_2.0");
       break;
     default:
       licenseBadge = renderLicenseBadge(data.license);
@@ -99,6 +99,6 @@ function generateMarkdown(data) {
   ## Questions
   If you have any questions, please feel free to reach out to me on [GitHub](https://github.com/${data.githubUsername}) or by email at ${data.email}.
   `;
-  }
+}
 
 module.exports = generateMarkdown;
